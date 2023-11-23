39af1883158dfc42d16869bfa084e82e52e61eb9
KimJi-An <j0012a@naver.com>
2023-11-23 19:38:41 +0900

    코인 이미지 넣기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,47 @@
-import { useParams } from "react-router";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
+interface RouteState {
+  name: string;
+}
+
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
   return (
-    <h1>Coin: {coinId}</h1>
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading"}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
   );
 }
 
